ee89b4026309891f955a7a495e515d59db5bb4da
yen <justhandsome1127@gmail.com>
2025-04-11 02:14:55 +0800

    Initialnioitial commit: check online

---
+++ chiikawa.py
@@ -0,0 +1,37 @@
+import re
+import string
+import discord
+import random
+import json
+import time
+import asyncio
+import datetime
+from discord.ext import commands,tasks
+import os
+from dotenv import load_dotenv
+
+# load .env variable
+load_dotenv()
+key = os.getenv("DC_KEY")
+print(f"successful read key : {key}")
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+#when bot start
+async def on_ready():
+    print('目前登入身份：',client.user)
+    
+@client.event
+#when message
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content == 'test':
+        await message.channel.send("hi")
+    
+    
+client.run(DC_KEY)
